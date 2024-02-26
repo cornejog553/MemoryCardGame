74f9de1d1c7ddeab4ea6f0c6d5f9d3e7cc7b8563
Giovanni Cornejo <cornejog553@gmail.com>
2024-02-26 16:08:03 -0500

    added full functionality. Shuffle, score keeping and game mechanics added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import './App.css'
 
 function App() {
 
+  const [highScore, setHighScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [clickedImgIds, setClickedImgIds] = useState([]);
+
+
     // Setting state for photos array
     const [photos, setPhotos] = useState([]);
 
@@ -18,15 +23,40 @@ function App() {
         });
     }, []);
 
+    // useEffect so that high score is able to be displayed accurately with current score
+    useEffect(()=>{
+      if(currentScore > highScore){
+        setHighScore(currentScore)
+      }
+    }, [currentScore, highScore])
 
-    // Function that will shuffle the photos. To be used with on click of each image
-    const shufflePhotos = () => {
-      for (let i = photos.length; --i;)  {
+
+    // function that runs when clicking an image. Checks to see if image id is part of clicked image array
+    const imgClicked = (id) => {
+      if(clickedImgIds.find((photoId) => photoId === id)){
+        console.log("Game OVer");
+        setCurrentScore(0)
+
+        // Function that will shuffle the photos. To be used with on click of each image
+        for (let i = photos.length; --i;)  {
           let j = Math.floor(Math.random() * (i + 1));
           [photos[i], photos[j]] = [photos[j], photos[i]]
       }
       setPhotos([...photos]);
-  }
+      } else {
+        const replacementClickedList = [...clickedImgIds, id];
+        setClickedImgIds(replacementClickedList);
+        setCurrentScore(currentScore + 1)
+        console.log(currentScore);
+        
+        // Function that will shuffle the photos. To be used with on click of each image
+        for (let i = photos.length; --i;)  {
+          let j = Math.floor(Math.random() * (i + 1));
+          [photos[i], photos[j]] = [photos[j], photos[i]]
+      }
+      setPhotos([...photos]);
+      }
+    }
 
   return (
     <>
@@ -34,13 +64,16 @@ function App() {
       <p>
         Get points by clicking on an image but dont click on any more than once
       </p>
+
+      <p>Current Score: {currentScore}</p>
+      <p>High Score: {highScore}</p>
+
       {/* Mapping the photos array */}
       {photos.map((photo) => (
-        <img key={photo.id} src={photo.urls.small} alt={photo.description} height={300} onClick={shufflePhotos}/>
+        <img key={photo.id} src={photo.urls.small} alt={photo.description} height={300} onClick={()=>{imgClicked(photo.id)}}/>
       ))}
     </>
   )
 }
-
 
 export default App
